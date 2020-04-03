480e674a2d31c6e68d8bb314a908dce1d4f302d4
Saifullah <hexters@gmail.com>
2020-04-03 22:13:10 +0700

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,4 @@
 const mix = require('laravel-mix');
 
 mix.js('src/js/app.js', 'dist/js')
-  .sass('src/sass/app.scss', 'dist/css');;
-
-mix.browserSync('asptemplate.dep');
+  .sass('src/sass/app.scss', 'dist/css');
